9fa1d666841556d506458e370062ad670fc6837b
SeaHuyty <seahuyty4444@gmail.com>
2025-03-17 16:07:30 +0700

    Upload files

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+function App() {
+  return (
+    <>
+
+      <div className="container">
+          <nav className="navBar">
+            <a href="#">HOME</a>
+            <a href="#">HOME</a>
+            <a href="#">HOME</a>
+            <a href="#">HOME</a>
+          </nav>
+
+          <footer>
+            <a href="#">HOME</a>
+            <a href="#">HOME</a>
+            <a href="#">HOME</a>
+            <a href="#">HOME</a>
+          </footer>
+
+          <section className="one">
+            <h1>SEA HUYTY</h1>
+            <img src="car1.png" alt="car" />
+          </section>
+
+          <section className="two">
+              <h1>Second Page</h1>
+          </section>
+
+          <section className="three">
+              <h1>Third Page</h1>
+          </section>
+
+          <section className="four" id="fi">
+              <h1>Fourth Page</h1>
+          </section>
+
+          <section className="five">
+              <h1>Fifth Page</h1>
+          </section>
+
+      </div>
+    </>
+  );
+};
+
+export default App;
